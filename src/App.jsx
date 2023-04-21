6b474e425463e6dc17206270de631c9466e0c59d
yurehn <d.carrascoquinchn@uandresbello.edu>
2023-04-20 21:37:55 -0400

    se creo base de diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react"
+import "./index.css"
+
+// const calculate = (value) => {
+//   return value
+// }
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [value, setValue] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="PanelTheme">
+        <span>calc</span>
+        <div>
+          <span>Theme</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <input className="input" type="text" disabled="disabled" value='399,981' />
+      <div className="wrapperButtons">
+        <button className="numberButton">
+          <span>7</span>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
